4d447e411dafa606e95ffbb968a98f1a69e450da
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-03-23 11:09:12 +0100

    Updates

---
+++ precache-manifest.66639b2da4f94e9d5a9ec7a6d128024f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4afd8c3008ab35db3c4041da1a9352de",
+    "url": "/product-landing-page/index.html"
+  },
+  {
+    "revision": "03b2b2d376eda88dcef2",
+    "url": "/product-landing-page/static/css/main.fb10dd5b.chunk.css"
+  },
+  {
+    "revision": "16b228ae4b2b80c357e9",
+    "url": "/product-landing-page/static/js/2.f190af51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/product-landing-page/static/js/2.f190af51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03b2b2d376eda88dcef2",
+    "url": "/product-landing-page/static/js/main.3b09b72c.chunk.js"
+  },
+  {
+    "revision": "cfdc49866b2952513d1d",
+    "url": "/product-landing-page/static/js/runtime-main.4af08422.js"
+  }
+]);
